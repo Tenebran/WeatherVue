cf6ab25e6692fb0c0fa36c94eb1ae25d2978f9c9
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-06 21:49:09 +0100

    feat: add Vuetify library with toolbar and inputs for email, password, and name

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script lang="ts">
 import { v1 } from 'uuid';
 import { defineComponent } from 'vue';
+import User from './components/User.vue';
 
-type userData = { name: string; email: string; password: string; id: string }[];
+export type userData = { name: string; email: string; password: string; id: string };
 
 export default defineComponent({
+  components: { User },
   data(this: { info: string; subtitle: string }) {
     return {
-      users: [] as userData,
+      users: [] as userData[],
       userName: '',
       userPassword: '',
       userEmail: '',
@@ -39,34 +41,75 @@ export default defineComponent({
       this.userPassword = '';
       this.userEmail = '';
     },
-    getUserData() {
-      // this.users.map(l => r)
+    deletUser(id: string) {
+      this.users = this.users.filter((el) => el.id !== id);
     },
   },
 });
 </script>
 
 <template>
-  <input type="text" v-model="userName" placeholder="Name" />
-  <input type="password" v-model="userPassword" placeholder="Password" />
-  <input type="email" v-model="userEmail" placeholder="Email" />
-  <span class="userInput">
-    <span class="error">{{ error }}</span></span
-  >
-  <div v-if="users.length === 0">You hav no Users</div>
+  <v-card color="grey-lighten-4" height="70px" rounded="0" flat>
+    <v-toolbar density="compact">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-  <div v-for="user in users" :key="user.id">
-    <div class="userInput">
-      <span class="userInputData">{{ user.name }}</span>
-    </div>
-    <div class="userInput">
-      <span class="userInputData">{{ user.email }}</span>
-    </div>
-    <div class="userInput">
-      <span class="userInputData">{{ user.password }}</span>
-    </div>
-  </div>
-  <button @click="sendData()">send</button>
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+
+  <v-container class="content-container">
+    <v-sheet class="bg-deep-purple pa-12" rounded>
+      <v-card class="mx-auto px-6 py-8" max-width="344">
+        <v-text-field
+          v-model="userEmail"
+          class="mb-2"
+          label="Email"
+          :error="!userEmail && !!error"
+          :errorMessages="!userEmail ? error : ''"
+          clearable></v-text-field>
+
+        <v-text-field
+          v-model="userName"
+          class="mb-2"
+          label="Name"
+          :error="!userName && !!error"
+          :errorMessages="!userName ? error : ''"
+          clearable></v-text-field>
+
+        <v-text-field
+          v-model="userPassword"
+          label="Password"
+          placeholder="Enter your password"
+          :error="!userPassword && !!error"
+          :errorMessages="!userPassword ? error : ''"
+          clearable></v-text-field>
+
+        <br />
+
+        <v-btn color="success" size="large" @click="sendData()" variant="elevated" block>
+          Sign In
+        </v-btn>
+      </v-card>
+    </v-sheet>
+
+    <div v-if="users.length === 0">You hav no Users</div>
+
+    <User v-for="user in users" :key="user.id" :user="user" :deletUser="deletUser" />
+  </v-container>
 </template>
 
 <style lang="scss" scoped>
@@ -92,5 +135,11 @@ input {
 
 .my-class {
   color: $primary-color;
+}
+
+.content-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 16px;
 }
 </style>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,13 +1,38 @@
-<template>
-  <div></div>
-</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { userData } from '../App.vue';
 
-<script>
-export default {
-  setup() {
-    return {};
+export default defineComponent({
+  props: {
+    user: {
+      type: Object as PropType<userData>,
+    },
+    deletUser: {
+      type: Function,
+    },
   },
-};
+  data() {
+    return;
+  },
+});
 </script>
 
-<style lang="scss" scoped></style>
+<template>
+  <div class="userInput">
+    <span class="userInputData">{{ user.name }}</span>
+  </div>
+  <div class="userInput">
+    <span class="userInputData">{{ user.email }}</span>
+  </div>
+  <div class="userInput">
+    <span class="userInputData">{{ user.password }}</span>
+  </div>
+  <button class="deleteUserButton" @click="deletUser(user.id)">Delete User</button>
+</template>
+
+<style scoped>
+.deleteUserButton {
+  display: block;
+  background-color: rgb(255, 59, 59);
+}
+</style>
