3bd2efcbe561ce279103433c671b88dd9a16f96a
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-07 21:20:06 +0100

    feat: added API request for weather data
    - Implemented API call to fetch weather information using Axios.
    - Integrated API response handling into the Weather component.
    - Updated logic to dynamically display weather data based on user input.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script lang="ts">
-import { v1 } from 'uuid';
 import { defineComponent, Ref, ref, watch } from 'vue';
-import User from './components/User.vue';
+import axios from 'axios';
 
 export type userData = { name: string; email: string; password: string; id: string };
 
@@ -9,6 +8,7 @@ export default defineComponent({
   setup() {
     const city = ref(localStorage.getItem('city') || '');
     let errorMessage: Ref<string, string> = ref('');
+    let weather = ref();
 
     watch(
       city,
@@ -23,11 +23,19 @@ export default defineComponent({
       if (city.value.trim().length < 2) {
         errorMessage.value = 'The name must be at least 2 characters long.';
         console.log('hello', errorMessage);
+      } else {
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=552fdd050682d4246694f7af40e8d829`
+          )
+          .then((resp) => (weather.value = resp.data))
+          .then((resp) => console.log(resp));
       }
     };
 
     const handleSearch = () => {
       console.log(`Searching weather for:`);
+      weather;
     };
 
     return {
@@ -35,6 +43,7 @@ export default defineComponent({
       errorMessage,
       handleSearch,
       getWeather,
+      weather,
     };
   },
 });
@@ -68,7 +77,75 @@ export default defineComponent({
           v-model="city"
           :disabled::append-inner="city.length <= 0"
           @click:append-inner="getWeather"></v-text-field>
-      </v-responsive> </v-card
+      </v-responsive>
+
+      <v-card class="mx-auto weather_card">
+        <v-card-item :title="weather.name">
+          <template v-slot:subtitle>
+            <img
+              class="me-1 pb-1"
+              :src="`https://openweathermap.org/img/wn/04n@2x.png`"
+              alt="Weather Icon"
+              style="width: 24px; height: 24px" />
+            {{ weather.weather[0].main }}
+          </template>
+        </v-card-item>
+
+        <v-card-text class="py-0">
+          <v-row align="center" no-gutters>
+            <v-col class="text-h2" cols="6"> {{ Math.floor(weather.main.temp) }}&deg;C </v-col>
+
+            <v-col class="text-right" cols="6">
+              <v-icon color="error" icon="mdi-weather-hurricane" size="88"></v-icon>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <div class="d-flex py-3 justify-space-between">
+          <v-list-item density="compact" prepend-icon="mdi-weather-windy">
+            <v-list-item-subtitle>123 km/h</v-list-item-subtitle>
+          </v-list-item>
+
+          <v-list-item density="compact" prepend-icon="mdi-weather-pouring">
+            <v-list-item-subtitle>48%</v-list-item-subtitle>
+          </v-list-item>
+        </div>
+
+        <v-expand-transition>
+          <div v-if="expand">
+            <div class="py-2">
+              <v-slider
+                v-model="time"
+                :max="6"
+                :step="1"
+                :ticks="labels"
+                class="mx-4"
+                color="primary"
+                density="compact"
+                show-ticks="always"
+                thumb-size="10"
+                hide-details></v-slider>
+            </div>
+
+            <v-list class="bg-transparent">
+              <v-list-item
+                class="weather_card"
+                v-for="item in forecast"
+                :key="item.day"
+                :append-icon="item.icon"
+                :subtitle="item.temp"
+                :title="item.day">
+              </v-list-item>
+            </v-list>
+          </div>
+        </v-expand-transition>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-btn></v-btn>
+        </v-card-actions>
+      </v-card> </v-card
   ></v-container>
 </template>
 
@@ -87,7 +164,11 @@ export default defineComponent({
     align-items: center;
     justify-content: center;
   }
-
+  &_card {
+    width: 50vw;
+    background: transparent;
+    padding: 0 20px;
+  }
   &_font {
     color: vars.$vue-green;
     &_subtitle {
