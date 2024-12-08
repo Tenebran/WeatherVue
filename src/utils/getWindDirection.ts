51d0ad066224f8f3172803ff802fedd3b8271d79
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-08 20:14:17 +0100

    Updates

--- src/utils/getWindDirection.ts
+++
@@ -1,6 +0,0 @@
-export const getWindDirection = (deg: number) => {
-  const directions = ['С', 'СВ', 'В', 'ЮВ', 'Ю', 'ЮЗ', 'З', 'СЗ'];
-
-  const index = Math.round(deg / 45) % 8;
-  return directions[index];
-};
